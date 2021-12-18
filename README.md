48343ca3aa7315ef411cbbd6fb295915e2f276f4
ethck <ethck0720@gmail.com>
2021-12-18 09:14:26 -0800

    Rewrote module to utilize monk's active tiles, sequencer, and tagger. Also made README for some additional help.

---
+++ README.md
@@ -0,0 +1,33 @@
+# JB2A Puzzle
+
+## Overview
+
+This module provides a compendium with several macros for use in creating a musical note password puzzle. In order to advance the players must match the code exactly to unlock some form of secret door.
+
+![sample passwords](media/puzzleSampleCodes.png)
+
+## Setup
+
+To setup the puzzle you simply need to enable the following modules and use our setup macro.
+
+-   Monk's Active Tiles
+-   Sequencer
+-   Tagger
+-   Any form of JB2A animated effects (technically only needs to be installed)
+
+You can find our setup macro in the compendium JB2A - Puzzle - Macros. Be sure to import all three macros into the world!
+When you execute the setup you should see a whole slew of tiles appear in the upper left hand corner of your screen like this:
+
+![sample setup screen](media/puzzleSetup.png)
+
+The musical notes are already setup with Monk's Active Tiles triggers and will play the associated animated effect when an actor steps on it. The pressure plate will automatically execute the pressure plate macro that you imported from the compendium.
+
+### Setting Up Doors
+
+The pressure plate is best accompanied with some secret doors that you want to be opened when the password is entered correctly. In order to set this up, you simply need to create a door (works with any type, doesn't have to be secret), then double right-click on the door to access its settings. In the settings at the bottom is a place for you to put tags. Simply add the `jb2a-door` tag and the pressure plate will automagically open and close the door when the password is entered correctly.
+
+![tagger door settings](media/puzzleDoorConfig.png)
+
+## Cleanup
+
+Are you done with the session where the puzzle was located, or don't want all of the macros in your macro directory anymore? Well, cleanup is as simple as using the JB2A - Puzzle Cleanup macro! This macro will delete all of the imported and auto-generated macros, tiles, journals, and folders from your world.
---
+++ scripts/JB2A_Pressure_Plate.js
@@ -0,0 +1,99 @@
+const music_folder = "modules/jb2a_patreon/Library/Generic/Music_Notation";
+const folder = await FilePicker.browse("data", music_folder);
+const files = folder.files;
+const animations = files.filter((file) => file.endsWith(".webm"));
+const tiles = files.filter((file) => file.endsWith(".webp"));
+
+const pressurePlate = Tagger.getByTag("jb2a-puzzle-pressure-plate")[0];
+const journalGM = game.journal.getName("GM Solution");
+const journalPlayer = game.journal.getName("PlayerEntry");
+
+const wait = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
+// only execute if the journalPlayer has the same note-code as the solution
+if (journalGM.data.content === journalPlayer.data.content) {
+    // Open the doors
+    // IMPORTANT: You must assign the doors the tag below for them to be toggled. By default it's `jb2a-door`
+    const doors = Tagger.getByTag("jb2a-door");
+    let doorUpdates = doors.map((door) => ({ _id: door.id, ds: 1 }));
+    await canvas.scene.updateEmbeddedDocuments("Wall", doorUpdates);
+
+    //This is the countdown timer section
+    journalPlayer.update({ content: `<h1>You have 5 seconds to exit the chamber</h1>` });
+    //We set an await, a delay in milliseconds, 1000 will roughly be equivalent to 1 second.
+    await wait(2000);
+    journalPlayer.update({ content: `<h1>4</h1>` });
+    await wait(1000);
+    journalPlayer.update({ content: `<h1>3</h1>` });
+    await wait(1000);
+    journalPlayer.update({ content: `<h1>2</h1>` });
+    await wait(1000);
+    journalPlayer.update({ content: `<h1>1</h1>` });
+    await wait(1000);
+    journalPlayer.update({ content: `<h1>0</h1>` });
+    await wait(1000);
+    journalPlayer.update({ content: ` ` });
+    // Close the doors
+    doorUpdates = doors.map((door) => ({ _id: door.id, ds: 0 }));
+    await canvas.scene.updateEmbeddedDocuments("Wall", doorUpdates);
+}
+
+// this executes regardless if we matched or not
+// at this point we reset the pattern
+journalPlayer.update({ content: `` });
+
+chosenTiles = [];
+
+// One of the Bass or BeamedQuaver
+chosenTiles.push(tiles.slice(0, 8)[Math.floor(Math.random() * 8)]);
+
+// One Flat or Crotchet
+chosenTiles.push(tiles.slice(8, 16)[Math.floor(Math.random() * 8)]);
+
+// Two Sharp or Treble Clef or Quavers
+chosenTiles.push(tiles.slice(16, 24)[Math.floor(Math.random() * 8)]);
+
+chosenTiles.push(tiles.slice(16, 24)[Math.floor(Math.random() * 8)]);
+
+let newContent = "";
+const cTile = pressurePlate;
+const scale = (cTile.data.width + cTile.data.height) / 2 / 100;
+
+// assemble new solution
+chosenTiles.forEach((tile) => {
+    //Class="secret means that this text/image will be only visible to the GM"
+    const content = `<img src = ${tile} width = 100 />`;
+    //Here, we add text to the Journal Entry
+    //journal.data.content is what's already in the Journal Entry and we add what we defined just above as "content"
+    newContent = newContent + content;
+});
+// update gm solution journal with new note-code
+journalGM.update({ content: newContent });
+
+// play the solution
+// prettier-ignore
+new Sequence()
+    .effect()
+        .atLocation(cTile)
+        .file(animations[tiles.indexOf(chosenTiles[0])])
+        .scale(scale)
+        .offset({ y: scale * 100 })
+    .wait(2000)
+    .effect()
+        .atLocation(cTile)
+        .file(animations[tiles.indexOf(chosenTiles[1])])
+        .scale(scale)
+        .offset({ y: scale * 100 })
+    .wait(2000)
+    .effect()
+        .atLocation(cTile)
+        .file(animations[tiles.indexOf(chosenTiles[2])])
+        .scale(scale)
+        .offset({ y: scale * 100 })
+    .wait(2000)
+    .effect()
+        .atLocation(cTile)
+        .file(animations[tiles.indexOf(chosenTiles[3])])
+        .scale(scale)
+        .offset({ y: scale * 100 })
+    .play();
